d69044164b588fd115d451cfe2992c0410f26c68
WANTEDHR <149701982+WANTEDHR@users.noreply.github.com>
2023-11-03 10:58:45 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ let id = GetID();
 export default function Home() {
   function start(){
       var playerid = document.getElementById("playernum").value;
-      if(!(playerid >0 && playerid <=14)) return;
+      if(!(playerid >=0 && playerid <=13)) return;
       else {
         /*Set("PLAYERID",playerid);*/
         Init(playerid);
